db4c1f4e58236279d80496b2c2533c06de436fc6
heartInsert <361846570@qq.com>
2022-03-02 17:38:10 +0800

    bboxannalysis

--- ObejectDetection/bbox_annalysis.py
+++ ObejectDetection/bbox_annalysis.py
@@ -1,22 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 
-bbox = pd.read_csv('train.csv', encoding='gbk')
+# 输入coco形式的 json 文件
+instance_json_path = '/home/xjz/Desktop/Coding/PycharmProjects/competition/Help_Tools/CV_tools/data/DcicCoco_train.json'
+with open(instance_json_path, 'r') as f:
+    json_file = json.load(f)
+annotations = json_file['annotations']
 width_list = []
 height_list = []
 width_height_ratio = []
-for index, row in bbox.iterrows():
-    bboxes = row['坐标'].split(";")
-    for bbox in bboxes:
-        x_mid, y_mid, width, height = bbox.split(' ')
-        width_list.append(float(width) * 256)
-        height_list.append(float(height) * 256)
-        width_height_ratio.append(float(width) * 256 / (float(height) * 256))
-        pass
-    pass
+for row in annotations:
+    x_min, y_min, width, height = row['bbox'].split(",")
+    width_list.append(width)
+    height_list.append(height)
+    width_height_ratio.append(width / height)
 plt.hist(width_list, bins=10, color='b', alpha=0.3)
-# plt.hist(height_list, bins=10, color='b', alpha=0.3)
-# plt.hist(width_height_ratio, bins=10, color='b', alpha=0.3)
+plt.hist(height_list, bins=10, color='b', alpha=0.3)
+plt.hist(width_height_ratio, bins=10, color='b', alpha=0.3)
 plt.show()
 print()
